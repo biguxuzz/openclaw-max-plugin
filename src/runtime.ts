6a0a9119a65e1dee5b2622a4573ef4b56740fa67
pavel.gorodilov <pavel.gorodilov@beringpro.ru>
2026-03-13 00:31:13 +0300

    fix 010

--- src/runtime.ts
+++ src/runtime.ts
@@ -197,6 +197,7 @@ class MaxRuntimeImpl {
             await this.sendMessage(userId, payload.text);
           }
         },
+        markComplete: () => {},
       };
 
       if (this.onMessage) {
